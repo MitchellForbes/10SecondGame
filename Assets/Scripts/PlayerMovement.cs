906876f276612012c820e25f3f0b831b6d0e9137
SAEStudentNathanHoward <114543371+SAEStudentNathanHoward@users.noreply.github.com>
2023-06-10 21:06:35 +1000

    Changed the PlayerCharacter prefab, added an image, added animations to it

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,16 +9,24 @@ public class PlayerMovement : MonoBehaviour
     public float jumpingPower = 8f; // charcter jump power
     private float horizontal; // used to get the horizontal inputs
 
+    [Header("")]
     [SerializeField] private Transform groundCheck; // used to check if ground is under the player
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private LayerMask groudLayer;
 
+    // Declaration of the animations for player movement
+    [Header("Character Animations")]
+    [SerializeField] private Animator playerCharacterAnimator;
+    [SerializeField] private AnimationClip characterIdleAnimation;
+    [SerializeField] private AnimationClip characterRunAnimation;
 
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal"); // user input to see if they go left or right
+        UpdateCharAnimation();
+       
 
         if (Input.GetButtonDown("Jump") && IsGrounded()) // allows the character to jump when space is pressed and when if there is ground under the player
         {                                                // then changes height based on time space is held
@@ -33,11 +41,31 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(horizontal * Speed, rb.velocity.y);  // Move the player horizontally based on input
+        UpdateCharAnimation();
     }
 
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groudLayer); // checks to see if anything is uder the player
 
+    }
+
+    private void UpdateCharAnimation()
+    {
+        if (horizontal == 1)
+        {
+            Debug.Log("Running right");
+            playerCharacterAnimator.SetBool("Running", true);
+
+        }
+        else if (horizontal == -1)
+        {
+            Debug.Log("Running left");
+            playerCharacterAnimator.SetBool("Running", true);
+        }
+        else
+        {
+            playerCharacterAnimator.SetBool("Running", false);
+        }
     }
 }
