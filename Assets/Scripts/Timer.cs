e6882bab4a4669c4394f30e345b9112920fcfafe
BunBaoi <135566105+BunBaoi@users.noreply.github.com>
2023-06-13 15:28:55 +1000

    Added Level Design and Collectible
    - 6 New levels
    - Collectibles that adds 1 sec when players collect

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -63,4 +63,14 @@ public class Timer : MonoBehaviour
     {
         timers = 0;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "collectible");
+        {
+            // Adds 1 sce to the timer upon colliding with the collectibles
+            timers += 1;
+            Destroy(other.gameObject); // destroy the prefab
+        }
+    }
 }
